022084e9a17f41732fbfa9140e0ec47673321138
rohitk959 <rohitkalaghatkar@gmail.com>
2016-11-06 03:11:50 +0530

    Modified MySQL JDBC Depricated Driver and added SSL = false

--- src/main/java/com/rohitrk/shaktigold/dao/impl/ItemDAOImpl.java
+++ src/main/java/com/rohitrk/shaktigold/dao/impl/ItemDAOImpl.java
@@ -4,9 +4,11 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.rohitrk.shaktigold.dao.ItemDAO;
 
+@Repository("itemDao")
 public class ItemDAOImpl implements ItemDAO {
 	
 	private JdbcTemplate jdbcTemplate;
--- src/main/java/com/rohitrk/shaktigold/service/impl/ItemServiceImpl.java
+++ src/main/java/com/rohitrk/shaktigold/service/impl/ItemServiceImpl.java
@@ -1,10 +1,12 @@
 package com.rohitrk.shaktigold.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.rohitrk.shaktigold.dao.ItemDAO;
 import com.rohitrk.shaktigold.service.ItemService;
 
+@Service("itemService")
 public class ItemServiceImpl implements ItemService {
 	
 	@Autowired
